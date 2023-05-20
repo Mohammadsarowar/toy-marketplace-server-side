410f7ea063f16ed45875f7b26d98c08fa03ebe82
Md Sarowar <mdsarowarhang@gmail.com>
2023-05-20 21:48:29 +0300

    catagory-save data

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 //middewore
- app.use(cors());
- app.use(express.json())
- require('dotenv').config()
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
 
- 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-o6iroya-shard-00-00.wymoxsw.mongodb.net:27017,ac-o6iroya-shard-00-01.wymoxsw.mongodb.net:27017,ac-o6iroya-shard-00-02.wymoxsw.mongodb.net:27017/?ssl=true&replicaSet=atlas-puyajh-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -27,15 +25,31 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const database = client.db("toyMarketplace").collection("allCar");
-    app.get('/allToy',async(req,res)=>{
-        const cursor = database.find();
-        const result = await cursor.toArray()
-        res.send(result) 
-    })
+    app.get("/allToy", async (req, res) => {
+      const cursor = database.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/allToy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await database.findOne(query);
+      res.send(result);
+    });
+    app.get("/allToy/:category", async (req, res) => {
+      console.log(req.params.id);
+      const toys = await database.find({
+          status: req.params.category
+        })
+        .toArray();
+      res.send(toys);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -43,12 +57,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("coffee making server is runing");
+});
 
-
- app.get('/',(req,res)=>{
-    res.send('coffee making server is runing')
- })
-
- app.listen(port,()=>{
-    console.log(`coffee server is running on port: ${port} `);
- })
+app.listen(port, () => {
+  console.log(`coffee server is running on port: ${port} `);
+});
